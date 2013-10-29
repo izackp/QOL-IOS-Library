bfada132585316c51e93fcfb9c2888ced40b9373
Isaac Paul <izackp@gmail.com>
2013-10-29 16:05:12 -0400

    Added a UITableViewCell+Shortcuts category. So creating a cell from a nib file is simple as createFromNib

---
+++ IsaacsIOSLibrary/Categories/UITableViewCell/UITableViewCell+Shortcuts.h
@@ -0,0 +1,15 @@
+//
+//  UITableViewCell+Shortcuts.h
+//  IsaacsIOSLibrary
+//
+//  Created by IsaacPaul on 10/29/13.
+//  Copyright (c) 2013 Isaac Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewCell (Shortcuts)
+
++ (instancetype)createFromNib;
+
+@end
