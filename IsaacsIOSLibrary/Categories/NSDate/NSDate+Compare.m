26015f18d1c230a1335bed5b06f5f1c2a805b6ce
Isaac Paul <izackp@gmail.com>
2013-10-30 14:42:53 -0400

    Added new functions to NSDate (Compare) category

--- IsaacsIOSLibrary/Categories/NSDate/NSDate+Compare.m
+++ IsaacsIOSLibrary/Categories/NSDate/NSDate+Compare.m
@@ -22,4 +22,38 @@
     return false;
 }
 
+- (bool)isToday {
+    NSDate* thisDate = [self dateYMD];
+    NSDate* todaysDate = [[NSDate date] dateYMD];
+    
+    return [thisDate isEqualToDate:todaysDate];
+}
+
+- (bool)isYesterday {
+    NSDate* thisDate = [self dateYMD];
+    
+    NSCalendar* cal = [NSCalendar currentCalendar];
+    NSDateComponents* offsetComp = [[NSDateComponents alloc] init];
+    offsetComp.day = -1;
+    NSDate* yesterday = [cal dateByAddingComponents:offsetComp toDate:thisDate options:0];
+    
+    return [thisDate isEqualToDate:yesterday];
+}
+
+- (NSDate*)dateYMD {
+    NSCalendar* cal = [NSCalendar currentCalendar];
+    NSDateComponents* ymdComps = [self componentsYMD];
+    return [cal dateFromComponents:ymdComps];
+}
+
+- (NSDateComponents*)componentsYMD {
+    NSCalendar* cal = [NSCalendar currentCalendar];
+    return [cal components:(NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay) fromDate:self];
+}
+
+- (NSDateComponents*)componentsOfTimeSinceDate:(NSDate*)date {
+    NSCalendar* cal = [NSCalendar currentCalendar];
+    return [cal components:(NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay) fromDate:date toDate:self options:0];
+}
+
 @end
