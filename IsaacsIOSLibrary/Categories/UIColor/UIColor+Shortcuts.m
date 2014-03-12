fd1548097ceb54c232279bc619c53e68c2c8c092
Isaac Paul <izackp@gmail.com>
2014-03-12 15:51:36 -0400

    Added a shortcut method to UIColor that produces a color with hexadecimal rgb values such as #FFAA01

--- IsaacsIOSLibrary/Categories/UIColor/UIColor+Shortcuts.m
+++ IsaacsIOSLibrary/Categories/UIColor/UIColor+Shortcuts.m
@@ -25,4 +25,9 @@
     return [self colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:alpha];
 }
 
++ (UIColor*)colorFromRGBHexValue:(NSInteger)hexValue withAlpha:(CGFloat)alpha {
+    
+    return [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0 green:((float)((hexValue & 0xFF00) >> 8))/255.0 blue:((float)(hexValue & 0xFF))/255.0 alpha:alpha];
+}
+
 @end
