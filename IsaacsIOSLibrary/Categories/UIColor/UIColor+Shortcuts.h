b8435f0e915383175d8f9b7c7081a4c55ff89b47
Isaac Paul <izackp@gmail.com>
2014-05-02 10:40:11 -0400

    Added uicolor to and from string methods to UIColor+Shortcuts

--- IsaacsIOSLibrary/Categories/UIColor/UIColor+Shortcuts.h
+++ IsaacsIOSLibrary/Categories/UIColor/UIColor+Shortcuts.h
@@ -19,8 +19,17 @@
 /*! Produces a color using red, green, and blue values that range from 0.0f to 255.0f, and Alpha ranges from 0.0f to 1.0f */
 + (UIColor*)color2WithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha;
 
-/*! Produces a color using a hexadecimal rgb value #FFAA01 and Alpha ranges from 0.0f to 1.0f */
+/*! Produces a color using a hexadecimal rgb value 0xFFAA01 */
++ (UIColor*)colorFromRGBHexValue:(NSInteger)hexValue;
+
+/*! Produces a color using a hexadecimal rgb value 0xFFAA01 and Alpha ranges from 0.0f to 1.0f */
 + (UIColor*)colorFromRGBHexValue:(NSInteger)hexValue withAlpha:(CGFloat)alpha;
+
+/*! Produces a color using the string representative of CIColor */
++ (UIColor*)colorFromString:(NSString*)color;
+
+/*! Produces the string representative of the CIColor */
+- (NSString*)stringValue;
 
 
 @end
