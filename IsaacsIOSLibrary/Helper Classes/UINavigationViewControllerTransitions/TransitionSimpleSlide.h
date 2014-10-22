819a248a5ecfc0cd402bb498b3e9d24c4898f616
Isaac Paul <izackp@gmail.com>
2014-10-22 17:26:17 -0400

    Added simple slide transition

---
+++ IsaacsIOSLibrary/Helper Classes/UINavigationViewControllerTransitions/TransitionSimpleSlide.h
@@ -0,0 +1,17 @@
+//
+//  TransitionSimpleSlide.h
+//  IsaacsIOSLibrary
+//
+//  Created by Isaac Paul on 10/22/14.
+//  Copyright (c) 2014 Isaac Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransitionSimpleSlide : NSObject
+
+@end
+
+@interface TransitionSimpleSlideReverse : NSObject
+
+@end
---
+++ IsaacsIOSLibrary/Helper Classes/UINavigationViewControllerTransitions/TransitionSimpleSlide.m
@@ -0,0 +1,70 @@
+//
+//  TransitionSimpleSlide.m
+//  IsaacsIOSLibrary
+//
+//  Created by Isaac Paul on 10/22/14.
+//  Copyright (c) 2014 Isaac Paul. All rights reserved.
+//
+
+#import "TransitionSimpleSlide.h"
+#import "UIView+Positioning.h"
+
+@implementation TransitionSimpleSlide
+
++ (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    return 0.33f;
+}
+
++ (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    UIViewController *toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    CGRect finalFrame = [transitionContext finalFrameForViewController:toViewController];
+    
+    UIView *containerView = [transitionContext containerView];
+    NSTimeInterval duration = [self transitionDuration:transitionContext];
+    
+    toViewController.view.frame = CGRectOffset(finalFrame, containerView.frame.size.width, 0);
+    
+    [containerView addSubview:toViewController.view];
+    
+    [UIView animateWithDuration:duration animations:^{
+        fromViewController.view.x = containerView.x - containerView.width;
+        toViewController.view.frame = finalFrame;
+    } completion:^(BOOL finished) {
+        [transitionContext completeTransition:YES];
+    }];
+}
+
+@end
+
+@implementation TransitionSimpleSlideReverse
+
++ (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    return 0.33f;
+}
+
++ (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    UIViewController *toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    CGRect toFinalFrame = [transitionContext finalFrameForViewController:toViewController];
+    
+    UIView *containerView = [transitionContext containerView];
+    NSTimeInterval duration = [self transitionDuration:transitionContext];
+    
+    
+    [containerView addSubview:toViewController.view];
+    [containerView sendSubviewToBack:toViewController.view];
+    
+    [UIView animateWithDuration:duration animations:^{
+        fromViewController.view.frame = CGRectOffset(fromViewController.view.frame, containerView.frame.size.width, 0);
+        toViewController.view.frame = toFinalFrame;
+    } completion:^(BOOL finished) {
+        [transitionContext completeTransition:YES];
+    }];
+}
+
+@end
