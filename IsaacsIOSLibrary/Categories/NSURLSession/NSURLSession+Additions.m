5032f1d90c9376899313e76430db067111d9e289
Isaac Paul <izackp@gmail.com>
2015-04-22 13:21:42 -0400

    Added Additions for NSURLSession

---
+++ IsaacsIOSLibrary/Categories/NSURLSession/NSURLSession+Additions.m
@@ -0,0 +1,56 @@
+//
+//  NSURLSession+Additions.m
+//  IsaacsIOSLibrary
+//
+//  Created by Isaac Paul on 4/22/15.
+//  Copyright (c) 2015 Isaac Paul. All rights reserved.
+//
+
+#import "NSURLSession+Additions.h"
+#import "NSObject+Shortcuts.h"
+
+@implementation NSURLSession (Additions)
+
++ (void)startWithRequest:(NSString*)url headers:(NSDictionary*)headers success:(void (^)(NSData *data, NSHTTPURLResponse *response))success failure:(void (^)(NSError* error))failure {
+    NSURLSession* session = [NSURLSession sharedSession];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
+    for (NSString* eachKey in [headers allKeys]) {
+        [request setValue:headers[eachKey] forHTTPHeaderField:eachKey];
+    }
+    
+    NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if ([response isKindOfClass:NSHTTPURLResponse.class] == false) {
+            if (failure) {
+                dispatch_sync(dispatch_get_main_queue(), ^{
+                    failure([self errorWithCode:0 andLocalizedDescription:@"Non HTTP Response"]);
+                });
+            }
+            return;
+        }
+        
+        if (error) {
+            if (failure) {
+                failure(error);
+            }
+        }
+        NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
+        if (success) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                success(data, httpResponse);
+            });
+        }
+    }];
+    [dataTask resume];
+}
+
++ (void)startWithJSONRequest:(NSString*)url headers:(NSDictionary*)headers success:(void (^)(NSHTTPURLResponse *response, NSDictionary* jsonData))success failure:(void (^)(NSError* error))failure {
+    [self startWithRequest:url headers:headers success:^(NSData *data, NSHTTPURLResponse *response) {
+        NSError* deserializeErr;
+        NSDictionary* jsonData = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&deserializeErr];
+        if (success) {
+            success(response, jsonData);
+        }
+    } failure:failure];
+}
+@end
