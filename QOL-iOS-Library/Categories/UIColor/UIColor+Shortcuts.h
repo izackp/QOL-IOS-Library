7e5b2e1a57095884f669d2df0b42d9a6691ab67b
Isaac Paul <izackp@gmail.com>
2017-11-30 15:45:37 -0500

    Updated UIColor to return nonnull

--- QOL-iOS-Library/Categories/UIColor/UIColor+Shortcuts.h
+++ QOL-iOS-Library/Categories/UIColor/UIColor+Shortcuts.h
@@ -11,25 +11,25 @@
 @interface UIColor (Shortcuts)
 
 /*! Produces a random color that isn't very white or very black */
-+ (UIColor *)randomColor;
++ (nonnull UIColor*)randomColor;
 
 /*! Produces a color using red, green, and blue values that range from 0.0f to 255.0f */
-+ (UIColor*)color2WithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue;
++ (nonnull UIColor*)color2WithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue;
 
 /*! Produces a color using red, green, and blue values that range from 0.0f to 255.0f, and Alpha ranges from 0.0f to 1.0f */
-+ (UIColor*)color2WithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha;
++ (nonnull UIColor*)color2WithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha;
 
 /*! Produces a color using a hexadecimal rgb value 0xFFAA01 */
-+ (UIColor*)colorFromRGBHexValue:(NSInteger)hexValue;
++ (nonnull UIColor*)colorFromRGBHexValue:(NSInteger)hexValue;
 
 /*! Produces a color using a hexadecimal rgb value 0xFFAA01 and Alpha ranges from 0.0f to 1.0f */
-+ (UIColor*)colorFromRGBHexValue:(NSInteger)hexValue withAlpha:(CGFloat)alpha;
++ (nonnull UIColor*)colorFromRGBHexValue:(NSInteger)hexValue withAlpha:(CGFloat)alpha;
 
 /*! Produces a color using the string representative of the CIColor */
-+ (UIColor*)colorFromString:(NSString*)color;
++ (nullable UIColor*)colorFromString:(nullable NSString*)color;
 
 /*! Produces the string representative of the CIColor */
-- (NSString*)stringValue;
+- (nonnull NSString*)stringValue;
 
 
 @end
