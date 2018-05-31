960eef26e23710df393fb7b2ef8fc5188090752c
Isaac Paul <izackp@gmail.com>
2018-05-31 15:46:05 -0400

    Added screensize extension

---
+++ QOL-iOS-Swiftly/Categories/UIScreen+Shortcuts.swift
@@ -0,0 +1,26 @@
+//
+//  UIScreen.swift
+//  QOL-iOS-Library
+//
+//  Created by Isaac Paul on 5/31/18.
+//  Copyright © 2018 Isaac Paul. All rights reserved.
+//
+
+import Foundation
+
+public extension UIScreen {
+    
+    public func screenSize() -> CGRect {
+        return screenSizeForOrientation(.portrait)
+    }
+    
+    public func screenSizeForOrientation(_ orientation:UIInterfaceOrientation) -> CGRect {
+        let size = bounds.size
+        var flip = orientation.isLandscape
+        if #available(iOS 8.0, *) {
+            let currentOrientation = UIApplication.shared.statusBarOrientation
+            flip = currentOrientation.isLandscape != orientation.isLandscape
+        }
+        return flip ? CGRect.init(x: 0, y: 0, width: size.height, height: size.width) : bounds
+    }
+}
