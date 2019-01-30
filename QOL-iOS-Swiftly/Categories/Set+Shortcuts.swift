beb90249f41c2be4365de3fd8c7f28d413aca3d6
Isaac Paul <izackp@gmail.com>
2019-01-30 17:45:45 -0500

    Added Set shortcuts

---
+++ QOL-iOS-Swiftly/Categories/Set+Shortcuts.swift
@@ -0,0 +1,15 @@
+//
+//  Set+Shortcuts.swift
+//  QOL-iOS-Swiftly
+//
+//  Created by Isaac Paul on 1/30/19.
+//  Copyright © 2019 Isaac Paul. All rights reserved.
+//
+
+import Foundation
+
+public extension Set {
+    public func array() -> Array<Element> {
+        return Array(self)
+    }
+}
