23a0466195d55ba1ada85ae8ae1fcb6024a1eaaa
Isaac Paul <izackp@gmail.com>
2019-09-11 11:49:44 -0400

    Added setAnchorPoint to UIView+Shortcuts. Added FloatingPointSign+Shortcuts flip() function.

--- QOL-iOS-Swiftly/Categories/UIView+Shortcuts.swift
+++ QOL-iOS-Swiftly/Categories/UIView+Shortcuts.swift
@@ -103,4 +103,23 @@ public extension UIView {
         edges.top = statusBarHeight
         return edges
     }
+    
+    func setAnchorPoint(anchorPoint: CGPoint) {
+        var newPoint = CGPoint.init(x: self.bounds.size.width * anchorPoint.x, y: self.bounds.size.height * anchorPoint.y)
+        var oldPoint = CGPoint.init(x: self.bounds.size.width * self.layer.anchorPoint.x, y: self.bounds.size.height * self.layer.anchorPoint.y)
+        
+        newPoint = newPoint.applying(self.transform)
+        oldPoint = oldPoint.applying(self.transform)
+        
+        var position = self.layer.position
+        
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+        
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+        
+        self.layer.position = position;
+        self.layer.anchorPoint = anchorPoint;
+    }
 }
